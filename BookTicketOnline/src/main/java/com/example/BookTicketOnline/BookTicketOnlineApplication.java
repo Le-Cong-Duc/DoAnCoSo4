45188f60d176770f0ab1cc556cbde696cc38c2c7
Le-Cong-Duc <congduc1801@gmail.com>
2025-10-30 20:44:14 +0700

    Start Project

---
+++ BookTicketOnline/src/main/java/com/example/BookTicketOnline/BookTicketOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookTicketOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTicketOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTicketOnlineApplication.class, args);
+	}
+
+}
---
+++ BookTicketOnline/src/test/java/com/example/BookTicketOnline/BookTicketOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookTicketOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTicketOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

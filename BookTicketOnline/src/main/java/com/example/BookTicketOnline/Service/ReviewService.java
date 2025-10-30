bf5700e08b90a730a5022985a3fbd3725307fad7
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-30 22:03:53 +0700

    update

--- BookTicketOnline/src/main/java/com/example/BookTicketOnline/Service/ReviewService.java
+++ BookTicketOnline/src/main/java/com/example/BookTicketOnline/Service/ReviewService.java
@@ -1,6 +1,5 @@
 package com.example.BookTicketOnline.Service;
 
-import com.example.BookTicketOnline.Repository.BookingRepository;
 import com.example.BookTicketOnline.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

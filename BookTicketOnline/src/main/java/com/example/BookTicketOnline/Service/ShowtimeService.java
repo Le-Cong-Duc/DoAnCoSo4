e2263185eeefe7a464a9fd2f3b44f99fd488db34
Le-Cong-Duc <congduc1801@gmail.com>
2025-11-19 19:55:35 +0700

    cinema - showtime

--- BookTicketOnline/src/main/java/com/example/BookTicketOnline/Service/ShowtimeService.java
+++ BookTicketOnline/src/main/java/com/example/BookTicketOnline/Service/ShowtimeService.java
@@ -1,11 +1,30 @@
 package com.example.BookTicketOnline.Service;
 
+import com.example.BookTicketOnline.Entity.Cinemas;
+import com.example.BookTicketOnline.Entity.Movies;
+import com.example.BookTicketOnline.Entity.Showtime;
 import com.example.BookTicketOnline.Repository.ShowtimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 public class ShowtimeService {
+
     @Autowired
-    private ShowtimeRepository showtimeRepository;
+    private ShowtimeRepository showTimeRepository;
+
+    public List<Showtime> getShowtimeByCinemaMovieAndDate(Integer cinemaId, Integer movieId, LocalDate date) {
+        return showTimeRepository.findByCinemaMovieAndDate(cinemaId, movieId, date);
+    }
+
+    public Showtime getShowtimeById(Integer showtimeId) {
+        return showTimeRepository.findById(showtimeId).orElse(null);
+    }
+
+    public List<Showtime> getShowtimesByMovie(Integer movieId) {
+        return showTimeRepository.findByMovieId(movieId);
+    }
 }
